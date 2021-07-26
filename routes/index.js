b3024bc417835c5e8bc754ef893fa7ffbb59605b
Rock-it-science <wilmc17@gmail.com>
2021-07-25 19:45:51 -0700

    index as default page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Hello');
+  res.sendfile('public/html/index.html');
 });
 
 module.exports = router;
