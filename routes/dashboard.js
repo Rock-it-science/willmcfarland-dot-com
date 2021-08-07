c70aeff0ff0339ca1969d2bc986d663d7d28dd3b
Will <wilmc17@gmail.com>
2021-08-06 23:46:53 -0700

    Progress on dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -11,7 +11,10 @@ var express = require('express');
 const { table } = require('console');
 var router = express.Router();
 
-//Query results
+
+/* Express functions */
+
+//Get pinned repositories
 router.get('/pinned', function(req, res, next){
     pinned_repos().then(result => {
         var repos = result.user.pinnedItems.nodes;
@@ -25,7 +28,7 @@ router.get('/pinned', function(req, res, next){
             <td>" + repos[i].description + "</td>\
             <td style=\"text-align:center;\">";
             for (var j = 0; j < repos[i].languages.nodes.length; j++){
-              row = row.concat("<p style=\"color: white; background-color:" + repos[i].languages.nodes[j].color + ";\">" + repos[i].languages.nodes[j].name + "</p><br/>");
+              row = row.concat("<p style=\"color: white; background-color:" + repos[i].languages.nodes[j].color + ";\">" + repos[i].languages.nodes[j].name + "</p>");
             }
             row = row.concat("</td>");
             for (var j = 0; j < repos[i].repositoryTopics.nodes.length; j++){
@@ -34,12 +37,66 @@ router.get('/pinned', function(req, res, next){
             row = row.concat("</td></tr>");
           tableText = tableText.concat(row);
         }
-        tableText = tableText.concat("\x27;")
+        tableText = tableText.concat("\x27);")
+        res.type('js');
         res.send(tableText);
       });
 });
 
+//Get most used languages
+router.get('/languages', function(req, res, next){
+  repo_languages().then(result => {
+    var repos = result.user.repositories.nodes;
+    
+    //First make an array of all languages in the result
+    var lang = [];
+    var l = "";
+    for(var i = 0; i < repos.length; i++){
+      for(var j = 0; j < repos[i].languages.edges.length; j++){
+        l = repos[i].languages.edges[j].node.name;
+        if(!(lang.includes(l))){
+          lang.push(l);
+        }
+      }
+    }
+
+    //Aggregate; sum size by language name
+    var size = [];
+    for(var i = 0; i < repos.length; i++){
+      for(var j = 0; j < repos[i].languages.edges.length; j++){//For every language in this repo
+        //Add to size total for this language
+        var lang_index = lang.indexOf(repos[i].languages.edges[j].node.name);
+        if(size[lang_index]){
+          size[lang_index] = size[lang_index] + repos[i].languages.edges[j].size;
+        } else{
+          size[lang_index] = repos[i].languages.edges[j].size;
+        }
+      }
+    }
+
+    //Percent of total
+    var total_size = 0;
+    for(var i=0; i<size.length; i++){
+      total_size = total_size + parseInt(size[l]);
+    }
+    var percent = [];
+    for(l in size){
+      percent.push(l / total_size * 100);
+    }
+
+    //Put it all in one multi-d array
+    lang_size_perc = [];
+    for(var i = 0; i < lang.length; i++){
+      lang_size_perc.push(lang[i], size[i], percent[i]);
+    }
+
+    res.type('js');
+    res.send("console.log(\x27" + total_size + "\x27)");
+  })
+});
+
 module.exports = router;
+
 
 /*GraphQL Queries */
 
@@ -96,4 +153,26 @@ async function pinned_repos() {
     }
   }
   `);
+}
+
+async function repo_languages() {
+  return await octokit.graphql(`
+  {
+    user(login: "Rock-it-science") {
+      repositories(first: 100) {
+        nodes {
+          name
+          languages(first: 100) {
+            edges {
+              node {
+                name
+              }
+              size
+            }
+          }
+        }
+      }
+    }
+  }
+  `)
 }
